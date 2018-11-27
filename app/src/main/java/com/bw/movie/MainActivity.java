a275390d845c0b6291548eb929cd21950d634582
Hk952700 <1622309994@qq.com>
2018-11-27 14:36:37 +0800

    焦浩康首页底部tab图片加入res

--- app/src/main/java/com/bw/movie/MainActivity.java
+++ app/src/main/java/com/bw/movie/MainActivity.java
@@ -1,7 +1,7 @@
 package com.bw.movie;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +9,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
 }
