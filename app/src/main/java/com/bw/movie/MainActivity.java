9695c86007f5abc7df078e06bdbb137a02c19b13
JiangGuXu <1324837706@qq.com>
2018-11-27 10:42:33 +0800

    赵瑜峰提交

--- app/src/main/java/com/bw/movie/MainActivity.java
+++ app/src/main/java/com/bw/movie/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //姜谷蓄测试--2018年11月27日 10:40:01
+        //赵瑜峰测试--2018年11月27日 10:42:01
     }
 }
