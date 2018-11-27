2d238d0faeba18e5ae054bda7d3c6cd2fdab3b9c
JiangGuXu <1324837706@qq.com>
2018-11-27 14:56:02 +0800

    添加了3个fragment

--- app/src/main/java/com/bw/movie/presenter_activity/MainActivityPresenter.java
+++ app/src/main/java/com/bw/movie/presenter_activity/MainActivityPresenter.java
@@ -1,10 +1,9 @@
-package com.bw.movie.presenter;
+package com.bw.movie.presenter_activity;
 
 import android.content.Context;
 
 import com.bw.movie.R;
 import com.bw.movie.mvp.view.AppDelage;
-import com.bw.movie.utils.net.SharedPreferencesUtils;
 
 public class MainActivityPresenter extends AppDelage {
     @Override
