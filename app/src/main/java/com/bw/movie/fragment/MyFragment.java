4b39fb25fa5feb52b4db5072ec7fe090329e5f5a
Hk952700 <1622309994@qq.com>
2018-11-27 15:02:25 +0800

    焦浩康添加了myfragment和删除了一个多余的mainactivity

--- app/src/main/java/com/bw/movie/fragment/MyFragment.java
+++ app/src/main/java/com/bw/movie/fragment/MyFragment.java
@@ -2,9 +2,15 @@ package com.bw.movie.fragment;
 
 import android.support.v4.app.Fragment;
 
+import com.bw.movie.mvp.presenter.BaseFragmentPresenter;
+import com.bw.movie.presenter_fragment.MyFragmentPresenter;
+
 /**
- *  我的页面
- *
+ * 我的页面
  */
-public class MyFragment extends Fragment{
+public class MyFragment extends BaseFragmentPresenter<MyFragmentPresenter> {
+    @Override
+    public Class<MyFragmentPresenter> getClassPresenter() {
+        return MyFragmentPresenter.class;
+    }
 }
--- app/src/main/java/com/bw/movie/presenter_fragment/MyFragmentPresenter.java
+++ app/src/main/java/com/bw/movie/presenter_fragment/MyFragmentPresenter.java
@@ -1,4 +1,18 @@
 package com.bw.movie.presenter_fragment;
 
-public class MyFragmentPresenter {
+import android.content.Context;
+
+import com.bw.movie.R;
+import com.bw.movie.mvp.view.AppDelage;
+
+public class MyFragmentPresenter extends AppDelage {
+    @Override
+    public int getLayoutId() {
+        return R.layout.fragment_my;
+    }
+
+    @Override
+    public void getContext(Context context) {
+
+    }
 }
