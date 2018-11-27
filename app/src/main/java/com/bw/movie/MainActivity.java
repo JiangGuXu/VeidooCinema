05738f96dfc95ee96fe3093780b6c5023e5640e1
MonsieurZhao <1261641750@qq.com>
2018-11-27 10:53:07 +0800

    小黑

--- app/src/main/java/com/bw/movie/MainActivity.java
+++ app/src/main/java/com/bw/movie/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //赵瑜峰测试--2018年11月27日 10:42:01
+        //小黑测试--2018年11月27日 10:42:01
     }
 }
