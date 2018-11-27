dbba0d02a776a0167fc9545182d901960cb9a395
JiangGuXu <1324837706@qq.com>
2018-11-27 13:45:53 +0800

    添加了判断网络的工具类

--- app/src/main/java/com/bw/movie/MainActivity.java
+++ app/src/main/java/com/bw/movie/MainActivity.java
@@ -3,13 +3,13 @@ package com.bw.movie;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.bw.movie.utils.NetworkAvalible;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
     }
 }
