6faab39e92829d5d482d2ce201287a53076d5907
JiangGuXu <1324837706@qq.com>
2018-11-27 14:54:20 +0800

    添加了3个fragment

---
+++ app/src/main/java/com/bw/movie/fragment/FilmFragment.java
@@ -0,0 +1,10 @@
+package com.bw.movie.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ *  电影页面
+ *
+ */
+public class FilmFragment extends Fragment{
+}
---
+++ app/src/main/java/com/bw/movie/fragment/MyFragment.java
@@ -0,0 +1,10 @@
+package com.bw.movie.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ *  我的页面
+ *
+ */
+public class MyFragment extends Fragment{
+}
