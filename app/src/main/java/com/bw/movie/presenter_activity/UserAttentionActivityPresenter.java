4d72f70c2f615b3734bf06ce7725422c81690f6d
Hk952700 <1622309994@qq.com>
2018-11-28 18:39:30 +0800

    焦浩康增加了用户信息页面

--- app/src/main/java/com/bw/movie/presenter_activity/UserAttentionActivityPresenter.java
+++ app/src/main/java/com/bw/movie/presenter_activity/UserAttentionActivityPresenter.java
@@ -5,6 +5,12 @@ import android.content.Context;
 import com.bw.movie.R;
 import com.bw.movie.mvp.view.AppDelage;
 
+
+/**
+ * 2018年11月28日 18:16:37
+ * 焦浩康
+ * 这是我的关注  activity
+ */
 public class UserAttentionActivityPresenter extends AppDelage {
 
     private Context context;
