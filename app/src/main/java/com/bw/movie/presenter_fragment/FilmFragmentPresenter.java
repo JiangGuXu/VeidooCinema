a95cadd64f932de9037d74562f4c7999dc8fb0e8
MonsieurZhao <1261641750@qq.com>
2018-11-28 13:32:06 +0800

    首页所需图片

--- app/src/main/java/com/bw/movie/presenter_fragment/FilmFragmentPresenter.java
+++ app/src/main/java/com/bw/movie/presenter_fragment/FilmFragmentPresenter.java
@@ -9,8 +9,12 @@ import com.bambootang.viewpager3d.LinearLocationTransformer;
 import com.bambootang.viewpager3d.LinearRotationTransformer;
 import com.bambootang.viewpager3d.LinearScaleTransformer;
 import com.bw.movie.R;
+import com.bw.movie.adapter.MyAdapterFilmList;
+import com.bw.movie.model.FilmList;
 import com.bw.movie.mvp.view.AppDelage;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 /*
@@ -23,7 +27,8 @@ public class FilmFragmentPresenter extends AppDelage {
 
     private Context context;
     private ListView mListView;
-
+    private MyAdapterFilmList myAdapterFilmList;
+    private List<FilmList> lists =new ArrayList<>();
     @Override
     public int getLayoutId() {
         return R.layout.fragment_film;
@@ -37,7 +42,25 @@ public class FilmFragmentPresenter extends AppDelage {
     @Override
     public void initData() {
         super.initData();
+        addlist();
         mListView = get(R.id.film_list_view);
+        myAdapterFilmList = new MyAdapterFilmList(context);
+        mListView.setAdapter(myAdapterFilmList);
+        myAdapterFilmList.setList(lists);
+    }
 
+    private void addlist() {
+        FilmList filmList = new FilmList();
+        filmList.setTitle("热门电影");
+        filmList.setUrl("/movieApi/movie/v1/findHotMovieList");
+        FilmList filmList1 = new FilmList();
+        filmList1.setTitle("正在热映");
+        filmList1.setUrl("/movieApi/movie/v1/findReleaseMovieList");
+        FilmList filmList2 = new FilmList();
+        filmList2.setTitle("即将上映");
+        filmList2.setUrl("/movieApi/movie/v1/findComingSoonMovieList");
+        lists.add(filmList);
+        lists.add(filmList1);
+        lists.add(filmList2);
     }
 }
