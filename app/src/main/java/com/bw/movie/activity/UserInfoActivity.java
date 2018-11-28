5a6ad51a85d82bd97d8bbd4a004ea6c67d672b45
Hk952700 <1622309994@qq.com>
2018-11-28 13:49:55 +0800

    焦浩康增加了用户信息页面

---
+++ app/src/main/java/com/bw/movie/activity/UserInfoActivity.java
@@ -0,0 +1,19 @@
+package com.bw.movie.activity;
+
+
+import com.bw.movie.mvp.presenter.BaseActivityPresenter;
+import com.bw.movie.presenter_activity.UserInfoActivityPresenter;
+
+/**
+ * 2018年11月28日 10:19:58
+ * 焦浩康
+ * 我的信息页面
+ * 例如my页面点击我的信息跳转这个页面
+ */
+public class UserInfoActivity extends BaseActivityPresenter<UserInfoActivityPresenter> {
+
+    @Override
+    public Class<UserInfoActivityPresenter> getClassPresenter() {
+        return UserInfoActivityPresenter.class;
+    }
+}
--- app/src/main/java/com/bw/movie/presenter_fragment/MyFragmentPresenter.java
+++ app/src/main/java/com/bw/movie/presenter_fragment/MyFragmentPresenter.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.bw.movie.R;
 import com.bw.movie.activity.LoginActivity;
 import com.bw.movie.activity.MainActivity;
+import com.bw.movie.activity.UserInfoActivity;
 import com.bw.movie.mvp.view.AppDelage;
 import com.bw.movie.utils.net.SharedPreferencesUtils;
 
@@ -27,6 +30,8 @@ public class MyFragmentPresenter extends AppDelage implements View.OnClickListen
 
     private Context context;
     private ImageView my_head_icon;
+    private RelativeLayout my_messiage_layout;
+    private Boolean isLogin;
 
     @Override
     public int getLayoutId() {
@@ -42,19 +47,37 @@ public class MyFragmentPresenter extends AppDelage implements View.OnClickListen
     public void initData() {
         super.initData();
 
+        //头像图片
         my_head_icon = get(R.id.my_head_icon);
         my_head_icon.setOnClickListener(this);
+
+
+        //我的信息布局 图片加文字
+        my_messiage_layout = get(R.id.my_messiage_layout);
+        my_messiage_layout.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             //点击头像未登录跳转到登录页面  登录怎么都不做
             case R.id.my_head_icon:
-                Boolean isLogin = SharedPreferencesUtils.getBoolean(context, "isLogin");
-                if(!isLogin){
-                    Intent intent = new Intent(context,LoginActivity.class);
-                    ((MainActivity)context).startActivity(intent);
+                isLogin = SharedPreferencesUtils.getBoolean(context, "isLogin");
+                if (!isLogin) {
+                    Intent intent = new Intent(context, LoginActivity.class);
+                    (context).startActivity(intent);
+                }
+                break;
+
+            //点击我的信息未登录吐司 登录状态 跳转到个人信息页面
+            case R.id.my_messiage_layout:
+                isLogin = SharedPreferencesUtils.getBoolean(context, "isLogin");
+                if (!isLogin) {
+                    Toast.makeText(context, "您还没有登录哦~", Toast.LENGTH_SHORT).show();
+                } else {
+                    Intent intent = new Intent(context, UserInfoActivity.class);
+                    (context).startActivity(intent);
                 }
                 break;
         }
