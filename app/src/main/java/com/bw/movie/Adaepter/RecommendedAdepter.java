bb394f5698901990d3da5f3e0626a6170e84460d
chengdanni <906645652@qq.com>
2018-11-28 18:26:12 +0800

    解析推荐影院接口

---
+++ app/src/main/java/com/bw/movie/Adaepter/RecommendedAdepter.java
@@ -0,0 +1,68 @@
+package com.bw.movie.Adaepter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bw.movie.R;
+import com.bw.movie.bean.Recommendedbean;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecommendedAdepter extends RecyclerView.Adapter<RecommendedAdepter.sRecommendedAdepter> {
+    private List<Recommendedbean.Resultbean> list = new ArrayList<>();
+    private Context context;
+
+    public RecommendedAdepter(Context context, List<Recommendedbean.Resultbean> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public sRecommendedAdepter onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = View.inflate(context, R.layout.activity_recommended, null);
+        sRecommendedAdepter vi = new sRecommendedAdepter(view);
+        return vi;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull sRecommendedAdepter sRecommendedAdepter, int i) {
+        sRecommendedAdepter.text.setText(list.get(i).getName());
+        sRecommendedAdepter.address.setText(list.get(i).getAddress());
+    // sRecommendedAdepter.distance.setText(list.get(i).getDistance());
+        String images = list.get(i).getLogo();
+        String[] split = images.split("\\|");
+        Picasso.with(context).load(split[0]).fit().into(sRecommendedAdepter.recommendimg);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class sRecommendedAdepter extends RecyclerView.ViewHolder {
+
+        private final ImageView recommendimg;
+        private final TextView text;
+        private final TextView address;
+         TextView distance;
+
+        public sRecommendedAdepter(@NonNull View itemView) {
+            super(itemView);
+
+            recommendimg = (ImageView) itemView.findViewById(R.id.activity_recommendimg);
+            text = (TextView) itemView.findViewById(R.id.activity_text);
+            address = (TextView) itemView.findViewById(R.id.activity_address);
+            distance = (TextView) itemView.findViewById(R.id.activity_distance);
+
+        }
+    }
+
+}
--- app/src/main/java/com/bw/movie/presenter_fragment/CinemaFragmentPresenter.java
+++ app/src/main/java/com/bw/movie/presenter_fragment/CinemaFragmentPresenter.java
@@ -2,6 +2,8 @@ package com.bw.movie.presenter_fragment;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -11,12 +13,17 @@ import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationClient;
 import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocationListener;
+import com.bw.movie.Adaepter.RecommendedAdepter;
 import com.bw.movie.R;
+import com.bw.movie.bean.Recommendedbean;
 import com.bw.movie.mvp.view.AppDelage;
+import com.bw.movie.utils.net.HttpUtil;
+import com.google.gson.Gson;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 
 /*
  * 影院页面presenter
@@ -33,6 +40,7 @@ public class CinemaFragmentPresenter extends AppDelage implements AMapLocationLi
     //声明mlocationClient对象
     public AMapLocationClient mlocationClient;
     private String city;
+    private RecyclerView recyclerView;
 
     @Override
     public int getLayoutId() {
@@ -41,13 +49,14 @@ public class CinemaFragmentPresenter extends AppDelage implements AMapLocationLi
 
     @Override
     public void getContext(Context context) {
-        this.context =context;
+        this.context = context;
     }
 
     @Override
     public void initData() {
         super.initData();
         imageView = (ImageView) get(R.id.activity_loca);
+        recyclerView = (RecyclerView) get(R.id.activity_recyclerView);
         imageView.setOnClickListener(this);
         mlocationClient = new AMapLocationClient(context);
 
@@ -56,8 +65,27 @@ public class CinemaFragmentPresenter extends AppDelage implements AMapLocationLi
     }
 
     private void dohttp() {
+        String url = "/movieApi/cinema/v1/findAllCinemas";
         HashMap<String, String> map = new HashMap<>();
-//        map.put("")
+        map.put("page", "1");
+        map.put("count", "2");
+        new HttpUtil().get(url, map).result(new HttpUtil.HttpListener() {
+            @Override
+            public void success(String data) {
+                Recommendedbean json = new Gson().fromJson(data, Recommendedbean.class);
+                List<Recommendedbean.Resultbean> bean = json.getResult();
+                RecommendedAdepter shangweiAdapter = new RecommendedAdepter(context, bean);
+                LinearLayoutManager s = new LinearLayoutManager(context);
+                s.setOrientation(LinearLayoutManager.VERTICAL);
+                recyclerView.setLayoutManager(s);
+                recyclerView.setAdapter(shangweiAdapter);
+            }
+
+            @Override
+            public void fail(String data) {
+
+            }
+        });
     }
 
 
@@ -84,7 +112,7 @@ public class CinemaFragmentPresenter extends AppDelage implements AMapLocationLi
                 amapLocation.getAdCode();//地区编码
             } else {
                 //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
-                Log.e("AmapError","location Error, ErrCode:"
+                Log.e("AmapError", "location Error, ErrCode:"
                         + amapLocation.getErrorCode() + ", errInfo:"
                         + amapLocation.getErrorInfo());
             }
@@ -93,7 +121,7 @@ public class CinemaFragmentPresenter extends AppDelage implements AMapLocationLi
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.activity_loca:
                 //初始化定位参数
                 mLocationOption = new AMapLocationClientOption();
@@ -111,15 +139,10 @@ public class CinemaFragmentPresenter extends AppDelage implements AMapLocationLi
                 mlocationClient.startLocation();
                 //进行地理位置获取
                 imageView.setOnClickListener(this);
-                Toast.makeText(context,city+"",Toast.LENGTH_SHORT).show();
+                Toast.makeText(context, city + "", Toast.LENGTH_SHORT).show();
                 break;
         }
     }
-
-
-
-
-
 
 
 }
