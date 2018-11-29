fb5f5736954fc9331b9d18a2781bb9236996a74f
chengdanni <906645652@qq.com>
2018-11-29 20:15:09 +0800

    程丹妮提交

--- app/src/main/java/com/bw/movie/presenter_activity/NearActivitypersenter.java
+++ app/src/main/java/com/bw/movie/presenter_activity/NearActivitypersenter.java
@@ -102,7 +102,7 @@ public class NearActivitypersenter extends AppDelage {
         Map<String, String> map = new HashMap<>();
         map.put("cinemasId", cinemasId);
         map.put("movieId", mid);
-        new HttpUtil().get(url1, map).result(new HttpUtil.HttpListener() {
+        new HttpUtil().get(url1, map,null).result(new HttpUtil.HttpListener() {
             @Override
             public void success(String data) {
                 Detailsbean bean = new Gson().fromJson(data, Detailsbean.class);
@@ -129,7 +129,7 @@ public class NearActivitypersenter extends AppDelage {
         Map<String, String> map = new HashMap<>();
         map.put("page", "1");
         map.put("count", "10");
-        new HttpUtil().get(url, map).result(new HttpUtil.HttpListener() {
+        new HttpUtil().get(url, map,null).result(new HttpUtil.HttpListener() {
             @Override
             public void success(String data) {
                 Gson gson = new Gson();
