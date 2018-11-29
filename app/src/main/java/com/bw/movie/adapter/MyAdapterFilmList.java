bbe117342a9876d476db30bde7d56e95323a6216
MonsieurZhao <1261641750@qq.com>
2018-11-29 09:07:55 +0800

    赵瑜峰首页更新影片

--- app/src/main/java/com/bw/movie/adapter/MyAdapterFilmList.java
+++ app/src/main/java/com/bw/movie/adapter/MyAdapterFilmList.java
@@ -11,24 +11,23 @@ import android.widget.TextView;
 import com.bw.movie.R;
 import com.bw.movie.model.FilmList;
 import com.bw.movie.model.FilmListData;
-import com.bw.movie.utils.net.HttpUtil;
-import com.google.gson.Gson;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class MyAdapterFilmList extends RecyclerView.Adapter<MyAdapterFilmList.MyViewHodlerFilmList>{
     private Context context;
-    private List<FilmList> list = new ArrayList();
+    private List<FilmListData> list = new ArrayList();
+    private List<String> titles=new ArrayList<>();
     private MyAdapterFilmList_recyler myAdapterFilmList_recyler;
+
 
     public MyAdapterFilmList (Context context){
         this.context=context;
     }
-    public void setList(List<FilmList> list){
+    public void setList(List<FilmListData> list,List<String> titles){
         this.list=list;
+        this.titles=titles;
         notifyDataSetChanged();
     }
     @NonNull
@@ -41,35 +40,14 @@ public class MyAdapterFilmList extends RecyclerView.Adapter<MyAdapterFilmList.My
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHodlerFilmList myViewHodlerFilmList, int i) {
-        myViewHodlerFilmList.title.setText(list.get(i).getTitle());
+        myViewHodlerFilmList.title.setText(titles.get(i));
         myAdapterFilmList_recyler = new MyAdapterFilmList_recyler(context);
         StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.HORIZONTAL);
         myViewHodlerFilmList.recyclerView.setLayoutManager(staggeredGridLayoutManager);
         myViewHodlerFilmList.recyclerView.setAdapter(myAdapterFilmList_recyler);
-        dohttp(list.get(i).getUrl());
+        myAdapterFilmList_recyler.setList(list.get(i).getResult());
     }
-    private void dohttp(final String url) {
-        Map<String,String> map = new HashMap<>();
-        map.put("page","1");
-        map.put("count","10");
-        new HttpUtil().get(url,map).result(new HttpUtil.HttpListener() {
-            @Override
-            public void success(String data) {
-                Gson gson = new Gson();
-                FilmListData filmListData = gson.fromJson(data, FilmListData.class);
-                List<FilmListData.ResultBean> result = filmListData.getResult();
-                if(result.size()==0){
-                    dohttp(url);
-                }
-                myAdapterFilmList_recyler.setList(result);
-            }
 
-            @Override
-            public void fail(String data) {
-
-            }
-        });
-    }
 
     @Override
     public int getItemCount() {
