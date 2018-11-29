bd97d89b27122feea47f9b739339b57b00d53667
Hk952700 <1622309994@qq.com>
2018-11-29 17:34:05 +0800

    焦浩康增加了用户登陆和退出登录的时候头像和文字的更换

--- app/src/main/java/com/bw/movie/presenter_activity/UserAttentionActivityPresenter.java
+++ app/src/main/java/com/bw/movie/presenter_activity/UserAttentionActivityPresenter.java
@@ -14,6 +14,11 @@ import com.bw.movie.R;
 import com.bw.movie.activity.MainActivity;
 import com.bw.movie.activity.UserAttentionActivity;
 import com.bw.movie.mvp.view.AppDelage;
+import com.bw.movie.utils.net.HttpUtil;
+import com.bw.movie.utils.net.HttpUtils;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -27,6 +32,8 @@ public class UserAttentionActivityPresenter extends AppDelage implements View.On
     private RadioGroup my_attention_radiogroup;
     private RecyclerView my_attention_recyclerview;
     private ImageView my_attention_back;
+    private RadioButton radioButton1;
+    private RadioButton radioButton2;
 
     @Override
     public int getLayoutId() {
@@ -44,33 +51,52 @@ public class UserAttentionActivityPresenter extends AppDelage implements View.On
         my_attention_radiogroup = get(R.id.my_attention_radiogroup);
         my_attention_recyclerview = get(R.id.my_attention_recyclerview);
         my_attention_back = get(R.id.my_attention_back);
+
+        radioButton1 = get(R.id.my_attention_radiobutton1);
+        radioButton2 = get(R.id.my_attention_radiobutton2);
+
         my_attention_back.setOnClickListener(this);
 
         my_attention_recyclerview.setLayoutManager(new LinearLayoutManager(context));
 
 
-        my_attention_radiogroup.check(R.id.my_attention_radiobutton1);
         my_attention_radiogroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
-                RadioButton radioButton1 = get(R.id.my_attention_radiobutton1);
-                RadioButton radioButton2 = get(R.id.my_attention_radiobutton2);
+
                 switch (checkedId) {
                     case R.id.my_attention_radiobutton1:
                         radioButton1.setTextColor(Color.parseColor("#FFFFFF"));
                         radioButton2.setTextColor(Color.parseColor("#333333"));
 
+                        Map<String, String> map = new HashMap<>();
+                        map.put("page", 1 + "");
+                        map.put("count", 10 + "");
+                        new HttpUtil().get("/movieApi/movie/v1/verify/findMoviePageList", map).result(new HttpUtil.HttpListener() {
+                            @Override
+                            public void success(String data) {
+                                Log.i("jhktest", "success: " + data);
+                            }
+
+                            @Override
+                            public void fail(String data) {
+                                Log.i("jhktest", "fail: " + data);
+                            }
+                        });
+
+
+                        Log.i("jhktest", "onCheckedChanged: " + 111);
 
                         break;
                     case R.id.my_attention_radiobutton2:
                         radioButton1.setTextColor(Color.parseColor("#333333"));
                         radioButton2.setTextColor(Color.parseColor("#FFFFFF"));
-
+                        Log.i("jhktest", "onCheckedChanged: " + 222);
                         break;
                 }
             }
         });
-
+        radioButton1.setChecked(true);
 
     }
 
