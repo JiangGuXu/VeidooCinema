c8a2c03b6cb4fb74ec1233a622280545c4eb10a9
MonsieurZhao <1261641750@qq.com>
2018-11-29 15:25:52 +0800

    赵瑜峰提交

--- app/src/main/java/com/bw/movie/utils/net/BaseService.java
+++ app/src/main/java/com/bw/movie/utils/net/BaseService.java
@@ -7,6 +7,7 @@ import io.reactivex.Observable;
 import okhttp3.MultipartBody;
 import okhttp3.ResponseBody;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
