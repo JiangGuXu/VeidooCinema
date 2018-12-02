a2f66a01c73acec2928f98180d529c84cb68c975
JiangGuXu <1324837706@qq.com>
2018-12-02 20:40:20 +0800

    完成了影院下单功能

--- app/src/main/java/com/bw/movie/presenter_activity/SelectedSetActivityPresenter.java
+++ app/src/main/java/com/bw/movie/presenter_activity/SelectedSetActivityPresenter.java
@@ -2,15 +2,25 @@ package com.bw.movie.presenter_activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bw.movie.R;
 import com.bw.movie.activity.SelectedSetActivity;
 import com.bw.movie.bean.Detailsbean;
+import com.bw.movie.bean.OrderBean;
 import com.bw.movie.mvp.view.AppDelage;
+import com.bw.movie.utils.encrypt.MD5EncryptUtil;
+import com.bw.movie.utils.net.HttpUtil;
+import com.bw.movie.utils.net.SharedPreferencesUtils;
 import com.bw.movie.widget.SeatTable;
+import com.google.gson.Gson;
 
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -20,9 +30,12 @@ import java.io.Serializable;
 public class SelectedSetActivityPresenter extends AppDelage{
     private SeatTable seatTableView;
     private TextView play_time;
+    private TextView play_data;
     private TextView activity_name;
     private TextView activity_detailsname;
     private TextView movie_text;
+    private TextView total_price;
+    private ImageView pay_yes;
     @Override
     public int getLayoutId() {
         return R.layout.activity_selectedset;
@@ -41,8 +54,11 @@ public class SelectedSetActivityPresenter extends AppDelage{
         activity_name = get(R.id.activity_name);
         activity_detailsname = get(R.id.activity_detailsname);
         movie_text = get(R.id.movie_name);
+        total_price = get(R.id.total_price);
+        play_data = get(R.id.play_data);
+        pay_yes = get(R.id.pay_yes);
         //接收传值
-        Detailsbean.Resultbean result = (Detailsbean.Resultbean) ((SelectedSetActivity)context).getIntent().getSerializableExtra("result");
+        final Detailsbean.Resultbean result = (Detailsbean.Resultbean) ((SelectedSetActivity)context).getIntent().getSerializableExtra("result");
         String name = ((SelectedSetActivity) context).getIntent().getStringExtra("name");
         String address = ((SelectedSetActivity) context).getIntent().getStringExtra("address");
         String movie_name = ((SelectedSetActivity) context).getIntent().getStringExtra("movie_name");
@@ -52,7 +68,51 @@ public class SelectedSetActivityPresenter extends AppDelage{
         activity_detailsname.setText(address);//影院的地址
         seatTableView.setScreenName(result.getScreeningHall()+"荧幕");//设置屏幕名称
         movie_text.setText(movie_name);
-        seatTableView.setMaxSelected(3);//设置最多选中
+        seatTableView.setMaxSelected(4);//设置最多选中
+        //点击对号
+        pay_yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //先判断用户是否登录
+                if (SharedPreferencesUtils.getBoolean(context,"isLogin")){
+                    //如果登录的话获取用户的信息
+                    int userId = SharedPreferencesUtils.getInt(context, "userId");
+                    String sessionId = SharedPreferencesUtils.getString(context, "sessionId");
+                    //拼接头部接口
+                    Map<String,String> headMap = new HashMap();
+                    headMap.put("Content-Type","application/x-www-form-urlencoded");
+                    String suserId = String.valueOf(userId);//转化过的userId
+                    headMap.put("userId", suserId);
+                    headMap.put("sessionId",sessionId);
+                    //拼接接口需要的参数
+                    Map<String,String> map = new HashMap<>();
+                    String scheduleId = String.valueOf(result.getId());//排期id
+                    String amount="1";//购买数量(目前有问题)
+                    String all = suserId+scheduleId+amount+"movie";//拼接签名需要的的信息
+                    //通过md5加密签名
+                    String sign = MD5EncryptUtil.MD5(all);
+                    map.put("scheduleId", scheduleId);
+                    map.put("amount",amount);
+                    map.put("sign",sign);
+                    //请求下单的接口
+                    new HttpUtil().postHead("/movieApi/movie/v1/verify/buyMovieTicket",map,headMap).result(new HttpUtil.HttpListener() {
+                        @Override
+                        public void success(String data) {
+                            OrderBean orderBean = new Gson().fromJson(data, OrderBean.class);
+                            Toast.makeText(context,orderBean.getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+
+                        @Override
+                        public void fail(String data) {
+
+                        }
+                    });
+                }else {
+                    Toast.makeText(context,"登录后才能购票哦~",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
         //选座控件的点击监听
         seatTableView.setSeatChecker(new SeatTable.SeatChecker() {
             @Override
@@ -71,9 +131,9 @@ public class SelectedSetActivityPresenter extends AppDelage{
             }
             @Override
             public void checked(int row, int column) {
-
+                double price = result.getPrice();
+                total_price.setText(price*column+"");
             }
-
             @Override
             public void unCheck(int row, int column) {
 
