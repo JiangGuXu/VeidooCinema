7302aec3260bce155624134a78703c26b3bb0b84
JiangGuXu <1324837706@qq.com>
2018-12-03 17:00:15 +0800

    姜谷蓄完成了点击影院的影片查看详情和点击选座

--- app/src/main/java/com/bw/movie/presenter_activity/FindCinemaActivityPresenter.java
+++ app/src/main/java/com/bw/movie/presenter_activity/FindCinemaActivityPresenter.java
@@ -1,12 +1,14 @@
 package com.bw.movie.presenter_activity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
 
 import com.bw.movie.R;
 import com.bw.movie.activity.FindCinemaActivity;
+import com.bw.movie.activity.PlayCinemaActivity;
 import com.bw.movie.adapter.FindCinemaAdapter;
 import com.bw.movie.bean.Recommendedbean;
 import com.bw.movie.mvp.view.AppDelage;
@@ -29,6 +31,14 @@ public class FindCinemaActivityPresenter extends AppDelage{
     private TextView movie_name;
     private RecyclerView recyclerView;
     private FindCinemaAdapter adapter;
+    private List<Recommendedbean.Resultbean> bean;
+    private int movieId;
+    private String name;
+    private String director;
+    private String type;
+    private String time;
+    private String country;
+    private String logo;
 
     @Override
     public int getLayoutId() {
@@ -48,8 +58,13 @@ public class FindCinemaActivityPresenter extends AppDelage{
         recyclerView = get(R.id.activity_recyclerView);
         adapter = new FindCinemaAdapter(context);
         //获取传值
-        String name = ((FindCinemaActivity) context).getIntent().getStringExtra("movie_name");
-        int movieId = ((FindCinemaActivity) context).getIntent().getIntExtra("movieId", 22);
+        name = ((FindCinemaActivity) context).getIntent().getStringExtra("movie_name");
+        director = ((FindCinemaActivity) context).getIntent().getStringExtra("Director");
+        type = ((FindCinemaActivity) context).getIntent().getStringExtra("type");
+        time = ((FindCinemaActivity) context).getIntent().getStringExtra("time");
+        country = ((FindCinemaActivity) context).getIntent().getStringExtra("country");
+        logo = ((FindCinemaActivity) context).getIntent().getStringExtra("logo");
+        movieId = ((FindCinemaActivity) context).getIntent().getIntExtra("movieId", 22);
         //赋值电影名
         movie_name.setText(name);
         //请求接口
@@ -59,7 +74,7 @@ public class FindCinemaActivityPresenter extends AppDelage{
             @Override
             public void success(String data) {
                 Recommendedbean json = new Gson().fromJson(data, Recommendedbean.class);
-                List<Recommendedbean.Resultbean> bean = json.getResult();
+                bean = json.getResult();
                 adapter.setList(bean);
                 LinearLayoutManager s = new LinearLayoutManager(context);
                 s.setOrientation(LinearLayoutManager.VERTICAL);
@@ -70,6 +85,28 @@ public class FindCinemaActivityPresenter extends AppDelage{
             @Override
             public void fail(String data) {
 
+            }
+        });
+        adapter.setListener(new FindCinemaAdapter.ItemClickListener() {
+            @Override
+            public void onItemClick(int position) {
+                //获取点击条目的电影院信息
+                String address = bean.get(position).getAddress();
+                String cinema_name = bean.get(position).getName();
+                int id = bean.get(position).getId();
+                // 跳转影院的排期
+                Intent intent = new Intent(context, PlayCinemaActivity.class);
+                intent.putExtra("cinema_name", cinema_name);
+                intent.putExtra("address",address);
+                intent.putExtra("logo",logo);
+                intent.putExtra("movie_name",name);
+                intent.putExtra("movieId",movieId);
+                intent.putExtra("Director",director);
+                intent.putExtra("type",type);
+                intent.putExtra("time",time);
+                intent.putExtra("country",country);
+                intent.putExtra("cinemaId",id);
+                ((FindCinemaActivity)context).startActivity(intent);
             }
         });
     }
